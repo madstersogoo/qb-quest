dc155a9d787bcf34a9d9fe38f131395569a8f642
leaudevian <mastersogood@gmail.com>
2021-09-13 12:22:35 +0200

    clean spawn ped

--- client/main.lua
+++ client/main.lua
@@ -1,35 +1,54 @@
-local NPC_1 = { x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true}
-Citizen.CreateThread(function()
-  modelHash = GetHashKey("a_m_m_hasjew_01")
-  RequestModel(modelHash)
-  while not HasModelLoaded(modelHash) do
-       Wait(1)
-  end
-  createNPC_1() 
-end)
 
-function createNPC_1()
-	created_ped = CreatePed(0, modelHash , NPC_1.x,NPC_1.y,NPC_1.z - 1, NPC_1.rotation, NPC_1.NetworkSync)
-	FreezeEntityPosition(created_ped, true)
-	SetEntityInvincible(created_ped, true)
-	SetBlockingOfNonTemporaryEvents(created_ped, true)
-	TaskStartScenarioInPlace(created_ped, "WORLD_HUMAN_COP_IDLES", 0, true)
+local NPC ={
+	{modelHash = "a_m_m_hasjew_01"  x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true},
+	{modelHash = "s_m_y_dockwork_01" ,x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true}
+}
+
+function setupModel(model)
+  RequestModel(model)
+  while not HasModelLoaded(model) do
+    RequestModel(model)
+    Wait(0)
+  end
+  SetModelAsNoLongerNeeded(model)
 end
 
-local NPC_2 = { x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true}
-Citizen.CreateThread(function()
-  modelHash = GetHashKey("s_m_y_dockwork_01")
-  RequestModel(modelHash)
-  while not HasModelLoaded(modelHash) do
-       Wait(1)
-  end
-  createNPC_2() 
-end)
+function pedCreation(PedToCreate)
 
-function createNPC_2()
-	created_ped = CreatePed(0, modelHash , NPC_2.x,NPC_2.y,NPC_2.z - 1, NPC_2.rotation, NPC_2.NetworkSync)
-	FreezeEntityPosition(created_ped, true)
-	SetEntityInvincible(created_ped, true)
-	SetBlockingOfNonTemporaryEvents(created_ped, true)
-	TaskStartScenarioInPlace(created_ped, "WORLD_HUMAN_COP_IDLES", 0, true)
+		setupModel(GetHashKey(PedToCreate.modelHash))
+		pedCreated = CreatePed(4, PedToCreate.modelHash, PedToCreate.x, PedToCreate.y, PedToCreate.z, PedToCreate.heading, false, false)
+		SetEntityAsMissionEntity(pedCreated, true, true)
+		--SetPedShootRate(pedCreated,  1000)
+		--AddArmourToPed(pedCreated, 100)
+		--SetPedAlertness(pedCreated, 0)
+		--SetPedAccuracy(pedCreated, 100)
+		--SetPedCanSwitchWeapon(pedCreated, true)
+		--SetEntityHealth(pedCreated,  200)
+		--SetPedFleeAttributes(pedCreated, 0, 0)
+		--SetPedCombatAttributes(pedCreated, 46, true)
+		--SetPedCombatAbility(pedCreated,  2)
+		--SetPedCombatMovement(pedCreated,  0)
+		--SetPedCombatRange(pedCreated, 0)
+		--SetPedPathAvoidFire(inputPed,  1)
+		--SetPedPathCanUseLadders(inputPed,1)
+		--SetPedPathCanDropFromHeight(inputPed, 1)
+		--SetPedPathPreferToAvoidWater(inputPed, 1)
+		SetEntityInvincible(pedCreated, true)
+		--GiveWeaponToPed(pedCreated, GetHashKey("weapon_sawnoffshotgun"), 5000, false, false)
+
 end
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+		if (not generalLoaded) then
+			if NetworkIsHost() then
+				for i=1,#NPC ,1 do
+					pedCreation(NPC[i])
+					Citizen.Wait(10)
+				end
+			end
+			generalLoaded = true
+		end
+  end
+end)
