52f64b7f05abc04498318fc55a585f816b3cd21a
leaudevian <mastersogood@gmail.com>
2021-09-13 20:52:57 +0200

    clean

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,6 @@
 
-local NPC ={
-	{modelHash = "a_m_m_hasjew_01"  x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true},
+local NPC = {
+	{modelHash = "a_m_m_hasjew_01"  ,x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true},
 	{modelHash = "s_m_y_dockwork_01" ,x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true}
 }
 
