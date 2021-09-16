2263fc57be0628042a0a46495f0fe6fa0159a63f
leaudevian <mastersogood@gmail.com>
2021-09-16 17:45:55 +0200

    repsuh

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,13 @@
 
 local NPC = {
 	{modelHash = "a_m_m_hasjew_01"  ,x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true},
-	{modelHash = "s_m_y_dockwork_01" ,x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true}
+	{modelHash = "s_m_y_dockwork_01" ,x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true},
+	{modelHash = "g_f_y_families_01" ,x = -131.05, y = -1556.8, z = 34.27, rotation = 34.83, NetworkSync = true},
+	{modelHash = "g_m_y_famca_01" ,x = -130.35, y = -1556.09, z = 34.29, rotation = 93.28, NetworkSync = true},
+	{modelHash = "g_m_y_famca_01" ,x = -174.54, y = -1563.88, z = 35.36, rotation = 288.11, NetworkSync = true},
+	{modelHash = "g_m_y_famca_01" ,x = -174.53, y = -1562.97, z = 35.36, rotation = 243.34, NetworkSync = true},
+	{modelHash = "g_m_y_famdnf_01" ,x = -173.72, y = -1562.3, z = 35.36, rotation = 210.45, NetworkSync = true},
+	{modelHash = "g_m_y_famdnf_01" ,x = -132.07, y = -1556.67, z = 34.26, rotation = 320.1, NetworkSync = true}
 }
 
 TriggerEvent("qb-SpawnPed:client:Spawn", NPC)
