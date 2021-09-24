0edbca4e68d16c298458c8da05b3c22d525d1aa3
leaudevian <mastersogood@gmail.com>
2021-09-24 20:45:23 +0200

    armed ped

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,10 @@
 
-local NPC = {
+local NPC_Basic = {
 	{modelHash = "a_m_m_hasjew_01"  ,x = -480.81, y = 6266.04, z = 13.63, rotation = 160.65, NetworkSync = true},
 	{modelHash = "s_m_y_dockwork_01" ,x = -1475.35, y = -3284.2, z = 13.94, rotation = 141.17, NetworkSync = true},
+}
+
+local NPC_Armed = {
 	{modelHash = "g_f_y_families_01" ,x = -131.05, y = -1556.8, z = 34.27, rotation = 34.83, NetworkSync = true},
 	{modelHash = "g_m_y_famca_01" ,x = -130.35, y = -1556.09, z = 34.29, rotation = 93.28, NetworkSync = true},
 	{modelHash = "g_m_y_famca_01" ,x = -174.54, y = -1563.88, z = 35.36, rotation = 288.11, NetworkSync = true},
@@ -10,4 +13,5 @@ local NPC = {
 	{modelHash = "g_m_y_famdnf_01" ,x = -132.07, y = -1556.67, z = 34.26, rotation = 320.1, NetworkSync = true}
 }
 
-TriggerEvent("qb-SpawnPed:client:Spawn", NPC)
+TriggerEvent("qb-SpawnPed:client:Spawn_Basic", NPC_Basic)
+TriggerEvent("qb-SpawnPed:client:Spawn_Armed", NPC_Armed)
